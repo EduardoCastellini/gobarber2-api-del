fb3f54899a4ea08844efef896b64629257d27b16
Eduardo Castellini <eduardo.castellini@tecnospeed.com.br>
2020-05-20 21:12:40 -0300

    Add services

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,5 +1,5 @@
 import { isEqual } from 'date-fns';
-import Appointment from '../models/Appoinment';
+import Appointment from '../models/Appointment';
 
 interface CreateAppointmentDTO {
   provider: string;
