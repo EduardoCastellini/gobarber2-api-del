603745f847b8a2cd409cdc871be39df3eb292dc7
Eduardo Castellini <eduardo.castellini@tecnospeed.com.br>
2020-08-01 14:45:18 -0300

    Criando Testes

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,21 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '1233456',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1233456');
+  });
+
+  // it('should no be able to create two appointment on the same time', () => {});
+});
