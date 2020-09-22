997b380066ec0fce35b15a8ac83916275a3b7a26
Eduardo Castellini <eduardo.castellini@tecnospeed.com.br>
2020-09-22 18:59:43 -0300

    Add func para listar agendamentos do prestador

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,16 @@ import { Router } from 'express';
 
 import ensureAuthenticate from '@modules/users/infra/http/middlewares/ensureAuthenticate';
 import AppointmentsController from '../controller/AppointmentsController';
+import ProviderAppointmentsController from '../controller/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticate);
 
 appointmentsRouter.post('/', appointmentsController.create);
+
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
